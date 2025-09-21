52ccef18e91029d1b35a6296af9e8fe5bd9371b6
caio.tavares <caio.tavares@unesp.br>
2025-09-21 14:47:07 -0300

    feat: plans

--- src/routes/(marketing)/pricing/pricing_plans.ts
+++ src/routes/(marketing)/pricing/pricing_plans.ts
@@ -17,19 +17,33 @@ export const pricingPlans = [
     ],
   },
   {
-    id: "pro",
-    name: "Pro",
+    id: "paid",
+    name: "Pago",
     description:
-      "A versão completa do curriculo, com suporte ilimitado a função de exportação em PDF e importação automática do Lattes.",
+      "A versão do currículo com suporte ilimitado a função de exportação em PDF e anexo de documentos.",
     price: "R$14,99",
     priceIntervalName: "14 dias",
-    stripe_price_id: "",
-    stripe_product_id: "",
+    stripe_price_id: null,
+    stripe_product_id: null,
     features: [
       "Tudo no plano Grátis",
       "Exportações em PDF ilimitadas",
       "Importação automática do Lattes",
       "Suporte prioritário",
+    ],
+  },
+  {
+    id: "lattes",
+    name: "Lattes",
+    description:
+      "A versão completa do curriculo, com suporte ilimitado a função de exportação em PDF e anexo de documentos e importação automática do Lattes.",
+    price: "R$29,99",
+    priceIntervalName: "um mês",
+    stripe_price_id: null,
+    stripe_product_id: null,
+    features: [
+      "Tudo no plano Pago",
+      "Importação automática do Lattes",
     ],
   }
 ]
